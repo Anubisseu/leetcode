41e1bbf78167844c7fcf7f73aee6f13bc79a38cd
王彬 <wangbiu@foxmail.com>
2016-08-20 21:19:12 +0800

    添加习题 198 House Robber
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/house_robber_198/HouseRobber.java
@@ -0,0 +1,10 @@
+package cn.edu.seu.kse.anubis.house_robber_198;
+
+/**
+ * Created by 王彬 on 2016/8/20.
+ */
+public class HouseRobber {
+    public int rob(int[] nums) {
+        return 0;
+    }
+}
