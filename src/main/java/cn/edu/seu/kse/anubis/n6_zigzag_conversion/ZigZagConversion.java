219ce712ade693116b4630ed466fc4eaca08a37b
王彬 <wangbiu@foxmail.com>
2016-08-24 20:46:57 +0800

    创建ZigZagConversion算法文件
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/n6_zigzag_conversion/ZigZagConversion.java
@@ -0,0 +1,12 @@
+package cn.edu.seu.kse.anubis.n6_zigzag_conversion;
+
+/**
+ * Created by 王彬 on 2016/8/24.
+ */
+public class ZigZagConversion {
+    public String convert(String s, int numRows) {
+        int zline= (int) Math.ceil(numRows/2);
+
+        return null;
+    }
+}
---
+++ src/test/java/cn/edu/seu/kse/anubis/n6_zigzag_conversion/ZigZagConversionTest.java
@@ -0,0 +1,13 @@
+package cn.edu.seu.kse.anubis.n6_zigzag_conversion;
+
+import org.junit.Test;
+
+/**
+ * Created by 王彬 on 2016/8/24.
+ */
+public class ZigZagConversionTest {
+    @Test
+    public void test(){
+
+    }
+}
