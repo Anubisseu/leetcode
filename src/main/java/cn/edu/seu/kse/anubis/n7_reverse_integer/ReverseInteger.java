f0b0a32e17ee1443e2c94ae3ea57b24d086998d0
王彬 <wangbiu@foxmail.com>
2016-08-25 12:56:05 +0800

    Reverse Integer 完成
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

--- src/main/java/cn/edu/seu/kse/anubis/n7_reverse_integer/ReverseInteger.java
+++ src/main/java/cn/edu/seu/kse/anubis/n7_reverse_integer/ReverseInteger.java
@@ -5,7 +5,31 @@ package cn.edu.seu.kse.anubis.n7_reverse_integer;
  */
 public class ReverseInteger {
     public int reverse(int x){
+        if(x>0){
+            return reversePosiveInteger(x);
+        }else if(x<0){
+            return -reversePosiveInteger(-x);
+        }else {
+            return x;
+        }
+    }
 
-        return 0;
+    public int reversePosiveInteger(int x){
+        int res=0;
+
+        while (x!=0){
+            res+=x%10;
+            x/=10;
+
+            if(x!=0){
+                if(res!=0&&Integer.MAX_VALUE/res<10){
+                    res=0;
+                    break;
+                }
+                res*=10;
+            }
+        }
+
+        return res;
     }
 }
--- src/test/java/cn/edu/seu/kse/anubis/n7_reverse_integer/ReverseIntegerTest.java
+++ src/test/java/cn/edu/seu/kse/anubis/n7_reverse_integer/ReverseIntegerTest.java
@@ -9,6 +9,12 @@ public class ReverseIntegerTest {
 
     @Test
     public void test(){
+        int[] nums={1, 234,-4324,2333,-43244,0,2974803,1534236469};
 
+        ReverseInteger ri=new ReverseInteger();
+
+        for(int i:nums){
+            System.out.printf("origin %d, reverse %d %n", i, ri.reverse(i));
+        }
     }
 }
