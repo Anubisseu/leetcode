80e41bfc9d238022e9e247b5b2f9dafc9748036b
王彬 <wangbiu@foxmail.com>
2016-08-25 12:34:29 +0800

    创建ReverseInteger文件
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/n7_reverse_integer/ReverseInteger.java
@@ -0,0 +1,11 @@
+package cn.edu.seu.kse.anubis.n7_reverse_integer;
+
+/**
+ * Created by 王彬 on 2016/8/25.
+ */
+public class ReverseInteger {
+    public int reverse(int x){
+
+        return 0;
+    }
+}
---
+++ src/test/java/cn/edu/seu/kse/anubis/n7_reverse_integer/ReverseIntegerTest.java
@@ -0,0 +1,14 @@
+package cn.edu.seu.kse.anubis.n7_reverse_integer;
+
+import org.junit.Test;
+
+/**
+ * Created by 王彬 on 2016/8/25.
+ */
+public class ReverseIntegerTest {
+
+    @Test
+    public void test(){
+
+    }
+}
