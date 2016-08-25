3992c07444126dab61a66bce395266667c74ea4e
王彬 <wangbiu@foxmail.com>
2016-08-25 13:37:36 +0800

    完成 Valid Perfect Square
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

--- src/main/java/cn/edu/seu/kse/anubis/n367_valid_perfect_square/ValidPerfectSquare.java
+++ src/main/java/cn/edu/seu/kse/anubis/n367_valid_perfect_square/ValidPerfectSquare.java
@@ -1,11 +1,58 @@
 package cn.edu.seu.kse.anubis.n367_valid_perfect_square;
 
+import java.util.HashSet;
+import java.util.InputMismatchException;
+
 /**
  * Created by 王彬 on 2016/8/25.
  */
 public class ValidPerfectSquare {
     public boolean isPerfectSquare(int num){
+        boolean res=lowEndValidate(num);
+        if(res==false){
+            return res;
+        }
 
-        return false;
+        int powl=0;
+        int powr=1;
+        int i=0;
+
+        while (true){
+            i++;
+            powr=i*i;
+
+            if(powr/i!=i){
+                res=false;
+                break;
+            }
+
+            if(powl==num){
+                res=true;
+                break;
+            }else if(num < powr){
+                res=false;
+                break;
+            }
+
+            powl=powr;
+        }
+
+
+        return res;
+    }
+
+    public boolean lowEndValidate(int num){
+        int low=num%10;
+        int[] nums={0,1,4,5,6,9};
+        HashSet<Integer> valid=new HashSet<>();
+        for(int n:nums){
+            valid.add(n);
+        }
+
+        if(valid.contains(low)){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
--- src/test/java/cn/edu/seu/kse/anubis/n367_valid_perfect_square/ValidPerfectSquareTest.java
+++ src/test/java/cn/edu/seu/kse/anubis/n367_valid_perfect_square/ValidPerfectSquareTest.java
@@ -8,6 +8,13 @@ import org.junit.Test;
 public class ValidPerfectSquareTest {
     @Test
     public void test(){
+        int[] nums={0,1,3,423423432,144,10000,232400001,93025};
+
+        ValidPerfectSquare vpf=new ValidPerfectSquare();
+
+        for(int n:nums){
+            System.out.printf("origin %d, sqrt %f, isPF %b%n", n, Math.sqrt(n),vpf.isPerfectSquare(n));
+        }
 
     }
 
