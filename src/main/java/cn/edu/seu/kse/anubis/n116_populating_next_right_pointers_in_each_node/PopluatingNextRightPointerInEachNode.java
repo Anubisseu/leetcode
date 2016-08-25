ff4e1119ca27608f3259f69eed9a13dfb17d4fe0
王彬 <wangbiu@foxmail.com>
2016-08-25 15:26:28 +0800

    创建Populating Next Right Pointer in each Node 文件
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/n116_populating_next_right_pointers_in_each_node/PopluatingNextRightPointerInEachNode.java
@@ -0,0 +1,21 @@
+package cn.edu.seu.kse.anubis.n116_populating_next_right_pointers_in_each_node;
+
+/**
+ * Created by 王彬 on 2016/8/25.
+ */
+public class PopluatingNextRightPointerInEachNode {
+    public void connect(TreeLinkNode root) {
+
+    }
+}
+
+
+
+class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
