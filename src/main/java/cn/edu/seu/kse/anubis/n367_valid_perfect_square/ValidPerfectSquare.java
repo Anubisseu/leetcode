df1b5219fe97e79c67aeb0b5f7e46807659e08e6
王彬 <wangbiu@foxmail.com>
2016-08-25 13:08:45 +0800

    创建 Valid  Perfect Square 算法文件
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/n367_valid_perfect_square/ValidPerfectSquare.java
@@ -0,0 +1,11 @@
+package cn.edu.seu.kse.anubis.n367_valid_perfect_square;
+
+/**
+ * Created by 王彬 on 2016/8/25.
+ */
+public class ValidPerfectSquare {
+    public boolean isPerfectSquare(int num){
+
+        return false;
+    }
+}
---
+++ src/test/java/cn/edu/seu/kse/anubis/n367_valid_perfect_square/ValidPerfectSquareTest.java
@@ -0,0 +1,14 @@
+package cn.edu.seu.kse.anubis.n367_valid_perfect_square;
+
+import org.junit.Test;
+
+/**
+ * Created by 王彬 on 2016/8/25.
+ */
+public class ValidPerfectSquareTest {
+    @Test
+    public void test(){
+
+    }
+
+}
